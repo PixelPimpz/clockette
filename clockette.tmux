046f9b3d7bfdc386926c8573334b970e368e063c
lucifer-morningstar <slobeck@gmail.com>
2026-02-06 18:48:25 -0800

    initial commit

---
+++ clockette.tmux
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PID_FILE="/tmp/clockette.pid"
+LOCAL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$( tmux show -gqv @CHER )/dump.fun"
+if [[ ! -f $PID_FILE ]]; then
+  tmux run-shell -b "$LOCAL_ROOT/scripts/clockette.sh"
+else
+  dump ">> Clockette is already running. \"Let it be.\" --The Beatles"
+  exit 0
+fi
---
+++ scripts/clockette.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+PID_FILE="/tmp/clockette.pid"
+source "$( tmux show -gqv @CHER )/dump.fun"
+source "$( tmux show -gqv @CHER )/fatal.fun"
+trap fatal EXIT
+
+main()
+{
+  echo "$$" > $PID_FILE
+  dump ">>> clockette: running. <prefix> + [C-x] to kill."
+  tmux bind C-x dump ">> Stopping clockette." \; tmux run-shell "kill -s KILL $(cat $PID_FILE )"
+}
+
+main
