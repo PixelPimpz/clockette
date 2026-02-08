d0d7df82ee1af14eb0722a1ba74e316c7c852781
lucifer-morningstar <slobeck@gmail.com>
2026-02-08 13:09:01 -0800

    Generic commit

--- scripts/clockette.sh
+++ scripts/clockette.sh
@@ -4,7 +4,9 @@ source "$SHARE/dump.fun"
 source "$SHARE/fatal.fun"
 dump ">>> scripts/clockette.sh running..."
 
-
-tmux set -g @clockette "%l#[blink]:#[noblink]%H%P"
-tmux source "$TMUX_ROOT/tmux.conf"
-tmux refresh-client
+main()
+{
+  echo "$$" > "/tmp/clockette.pid"
+  tmux set -g @clockette "%l#[blink]:#[noblink]%H%P"
+  tmux source "$TMUX_ROOT/tmux.conf"tmux refresh-client
+}
