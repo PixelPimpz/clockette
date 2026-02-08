0ad024abc8105cac47a1eb3b2a49a51d579f1763
lucifer-morningstar <slobeck@gmail.com>
2026-02-08 13:30:42 -0800

    Generic commit

--- scripts/killit.sh
+++ scripts/killit.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-  dump ">> killing clockette..."
-  kill -s KILL "$( cat $PID_FILE )"
-  [[ -f "$PID_FILE" ]] && rm -f "$PID_FILE" || fatal "/tmp/clockette.pid not found"
-  dump ">> /tmp/clockette.pid deleted."
+SHARE=$( tmux show -gqv @CHER )
+source "$SHARE/dump.fun"
+source "$SHARE/fatal.fun"
+
+dump ">> killing clockette..."
+kill -s KILL "$( cat $PID_FILE )"
+[[ -f "$PID_FILE" ]] && rm -f "$PID_FILE" || fatal "/tmp/clockette.pid not found"
+dump ">> /tmp/clockette.pid deleted."
