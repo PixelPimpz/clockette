ebbf109d439a8d0c915f48a09238175517bde139
lucifer-morningstar <slobeck@gmail.com>
2026-02-08 19:48:46 -0800

    Generic commit

--- scripts/killit.sh
+++ scripts/killit.sh
@@ -3,11 +3,16 @@ PID_FILE="/tmp/clockette.pid"
 SHARE=$( tmux show -gqv @CHER )
 source "$SHARE/dump.fun"
 source "$SHARE/fatal.fun"
+main()
+{
+  dump ">> killing clockette..."
+  local pid=$( cat "/tmp/clockette.pid" ) || pid=''
+  if [[ -n "$pid" && $pid =~ ^[0-9]+$ && kill -0 $pid 2>/dev/null ]]; then
+    kill -s TERM $pid && dump "clockette terminated"
+  else
+    dump "SIGTERM failed. Clockette was probably not running"
+  fi
+  rm -f "$PID_FILE" 2>/dev/null && dump ">> /tmp/clockette.pid deleted." 
+}
 
-dump ">> killing clockette..."
-pid=$( cat "/tmp/clockette.pid" )
-dump ">> $( cat /tmp/clockette.pid )" 
-dump ">> $( $pid )" 
-kill -s TERM $pid || fatal "SIGTERM failed"
-rm -f "$PID_FILE" || fatal "/tmp/clockette.pid not found"
-dump ">> /tmp/clockette.pid deleted."
+main
