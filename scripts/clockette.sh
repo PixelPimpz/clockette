f25cf8f13acdb9b9893e7b37a88263ef1d95180e
lucifer-morningstar <slobeck@gmail.com>
2026-02-13 13:56:45 -0800

    Generic commit

--- scripts/clockette.sh
+++ scripts/clockette.sh
@@ -10,9 +10,7 @@ main()
   echo "$$" > "/tmp/clockette.pid"
   while true; do
     read -r -a timedate <<< $( date "+%l %M %S %p %a %m %d %Y" )
-    local s2nm=$(( 60 - ${timedate[2]} ))
-    local m2nh=$(( 60 - ${timedate[1]} * 60 ))
-    local interval=$(( s2nm + m2nh ))
+    local interval=$(( 60 - ${timedate[2]} + 59 - ${timedate[1]} * 60 ))
     dump ">> interval: $interval"
     local clock_utf8=$( printf '\\U%X' "$(( CLOCK + "${timedate[0]}" - 1 ))" )
     local clock_icon=$( echo -e "$clock_utf8" )
