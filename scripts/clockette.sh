eb63c310745269dd0feb3ca17e2773992cc43b00
lucifer-morningstar <slobeck@gmail.com>
2026-02-13 23:47:25 -0800

    Generic commit

--- scripts/clockette.sh
+++ scripts/clockette.sh
@@ -10,15 +10,15 @@ main()
   echo "$$" > "/tmp/clockette.pid"
   while true; do
     read -r -a timedate <<< $( date "+%l %M %S %p %a %m %d %Y" )
-    #local seconds="10#${timedate[2]}"
-    #local minutes="10#${timedate[1]}"
     local s2nm=$(( ( 60 - "10#${timedate[2]}" ) % 60 )) 
     local m2nh=$(( 59 - "10#${timedate[1]}" )) 
     local interval=$(( s2nm + m2nh * 60 ))
+
     dump ">> interval: $interval"
-    local clock_utf8=$( printf '\\U%X' "$(( CLOCK + "${timedate[0]}" - 1 ))" )
-    local clock_icon=$( echo -e "$clock_utf8" )
-    tmux set -g @clockette "${clock_icon} %l#[blink]:#[noblink]%M%P"
+    #local clock_utf8=$( printf -e '\\U%X' "$(( CLOCK + "${timedate[0]}" - 1 ))" )
+    local clock=$( printf -e '\\U%X' "$(( CLOCK + "${timedate[0]}" - 1 ))" )
+    i#local clock_icon=$( echo -e "$clock_utf8" )
+    tmux set -g @clockette "${clock} %l#[blink]:#[noblink]%M%P"
     tmux refresh-client
     sleep 5
   done 
