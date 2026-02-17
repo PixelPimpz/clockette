a927d1165c0b6054f121e45bea5929b98e31b581
lucifer-morningstar <slobeck@gmail.com>
2026-02-17 12:28:56 -0800

    Generic commit

--- scripts/clockette.sh
+++ scripts/clockette.sh
@@ -10,13 +10,14 @@ main()
   echo "$$" > "/tmp/clockette.pid"
   while true; do
     read -r -a timedate <<< $( date "+%l %M %S %p %a %m %d %Y" )
+    (( timedate[0] > 9 )) && local SPC="" || local SPC=" "
     local s2nm=$(( ( 60 - "10#${timedate[2]}" ) % 60 )) 
     local m2nh=$(( 59 - "10#${timedate[1]}" )) 
     local interval=$(( s2nm + m2nh * 60 ))
 
     local clock_utf8=$( printf '\\U%X' "$(( CLOCK + "${timedate[0]}" - 1 ))" )
     local clock=$( echo -e "$clock_utf8" )
-    tmux set -g @clockette "${clock} %l#[blink]:#[noblink]%M%P"
+    tmux set -g @clockette "${clock}${SPC}%l#[blink]:#[noblink]%M%P"
     tmux set -g @today "${timedate[4]:0:2} %m/%d/%y"
     tmux refresh-client
     sleep $interval
